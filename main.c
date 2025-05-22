fdf11fa5f4a9f98eb279ea55477419f22ec785cb
Edson0824 <202503328226@alunos.estacio.br>
2025-05-22 12:08:59 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das Cartas
+// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
+// Siga os comentários para implementar cada parte do desafio.
+
+int main() {
+    int populacao1;
+    char codigo1[4];
+    int pontos_turisticos1;
+    float area1, pib1;
+
+    char codigo2[4];
+    int populacao2;
+    float area2, pib2;
+    int pontos_turisticos2;
+
+    printf("Cadastro da Carta 1\n");
+    printf("Digite o código (ex: A01): ");
+    scanf("%s", codigo1);
+    printf("População: ");
+    scanf("%d", &populacao1);
+    printf("Área (km²): ");
+    scanf("%f", &area1);
+    printf("PIB (em bilhões): ");
+    scanf("%f", &pib1);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos1);
+
+    printf("\n");
+
+    printf("Cadastro da Carta 2\n");
+    printf("Digite o código (ex: B02): ");
+    scanf("%s", codigo2);
+    printf("População: ");
+    scanf("%d", &populacao2);
+    printf("Área (km²): ");
+    scanf("%f", &area2);
+    printf("PIB (em bilhões): ");
+    scanf("%f", &pib2);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos2);
+
+    printf("\n");
+
+    printf("Carta 1 - %s\n", codigo1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões\n", pib1);
+    printf("Pontos turísticos: %d\n", pontos_turisticos1);
+
+    printf("\n");
+
+    printf("Carta 2 - %s\n", codigo2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões\n", pib2);
+    printf("Pontos turísticos: %d\n", pontos_turisticos2);
+
+    return 0;
+}
